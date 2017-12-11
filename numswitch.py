61ab9d499eb82bba65008e9b09249c22eea65aac
wwildridge19 <34096939+wwildridge19@users.noreply.github.com>
2017-12-11 11:17:35 -0500

    Update numswitch.py

--- numswitch.py
+++ numswitch.py
@@ -6,19 +6,20 @@ class numswitch():
 	def __init__ (self, message):
 		self.message = message
 
-def encode(): 
-	origlist = list(original)
-	listnum = []
-	for i in origlist:
-		listnum.append(ord(i)-97)
+	def encode(): 
+		origlist = list(original)
+		listnum = []
+		for i in origlist:
+			listnum.append(ord(i)-97)
+	def decode():
+		backtoorig = [] 
+		for i in listnum:
+			backtoorig.append(chr(i+97))
+
  
 print (listnum)
 
 #decoder
-def decode():
-	backtoorig = [] 
-	for i in listnum:
-		backtoorig.append(chr(i+97))
-
+	
 
 print(backtoorig)
