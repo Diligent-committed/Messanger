f8443e4cef49bf2e312fe671c35518754ef7006e
wwildridge19 <wwildridge19@choate.edu>
2017-12-11 11:07:21 -0500

    my numswitch

---
+++ numswitch.py
@@ -0,0 +1,24 @@
+import sys 
+
+#encoder
+original = str(input('what is your message'))
+class numswitch():
+	def __init__ (self, message):
+		self.message = message
+
+def encode(): 
+	origlist = list(original)
+	listnum = []
+	for i in origlist:
+		listnum.append(ord(i)-97)
+ 
+print (listnum)
+
+#decoder
+def decode():
+	backtoorig = [] 
+	for i in listnum:
+		backtoorig.append(chr(i+97))
+
+
+print(backtoorig)
